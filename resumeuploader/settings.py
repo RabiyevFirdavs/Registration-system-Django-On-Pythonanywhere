3e9507f74a1e59da6320e40028775655cbe3bf0d
Rabiyev Firdavs <firdavsrabiyev@gmail.com>
2023-11-17 00:01:17 +0300

    fedya

--- resumeuploader/settings.py
+++ resumeuploader/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@fvff8ywhw1h(xw__yps6^__b0f$-&cb-$tqai=^h*%cfb_mlg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
